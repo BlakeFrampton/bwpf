588c9e1da12dbffc11b622b3cef1c29808ec44fc
Daframp <blakeframps@icloud.com>
2024-10-03 11:52:55 +0100

    Failed at optimising menger fractal

--- Menger Sponge Fractal/sketch.js
+++ Menger Sponge Fractal/sketch.js
@@ -1,6 +1,11 @@
+
+
 let angle = 0;
 
 function setup() {
+
+
+  frameRate(60);
   angleMode(RADIANS);
   const width  = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
   const height = window.innerHeight|| document.documentElement.clientHeight|| document.body.clientHeight;
@@ -11,6 +16,7 @@ function setup() {
   let LayersLabel = createSpan('Layers');
   LayersLabel.position(width-95, 20);
 }
+
 function draw() {
   background(200);
   lights();
@@ -20,35 +26,28 @@ function draw() {
   rotate(angle * 1.2);
   rotateZ(angle * 0.3);
  
-  ambientMaterial(25, 50, 150);
-  specularMaterial(100, 50, 200);
-  shininess(180);
   subCube(min(width, height)/7, 1);
 
   angle += 0.01;
 }
 
 function subCube(BoxWidth, layer){
-  let ZeroOffsets = 0;
   for (let XOffset = -BoxWidth; XOffset <= BoxWidth; XOffset += BoxWidth){
     for (let YOffset = -BoxWidth; YOffset <= BoxWidth; YOffset += BoxWidth){
       for (let ZOffset = -BoxWidth; ZOffset <= BoxWidth; ZOffset += BoxWidth){
-        if (XOffset == 0){
-          ZeroOffsets += 1;
-        }
-        if (YOffset == 0){
-          ZeroOffsets += 1;
-        }
-        if (ZOffset == 0){
-          ZeroOffsets += 1;
-        }
-       if (ZeroOffsets < 2){
+        let isCenter = (XOffset == 0) + (YOffset == 0) + (ZOffset == 0);
+        if (isCenter < 2){
           push();
           translate(XOffset, YOffset, ZOffset);
           if (layer < LayersSlider.value()){
-          subCube(BoxWidth/3, layer + 1);
+            shininess(0)
+            specularMaterial(0,0,0)
+            subCube(BoxWidth/3, layer + 1);
           }else{
-          box(BoxWidth);
+            ambientMaterial(25, 50, 150);
+            specularMaterial(100, 50, 200);
+            shininess(180);
+            box(BoxWidth);
           }
           pop();
         }
