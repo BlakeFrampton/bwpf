caaa16f736e79391983fa3a2444fb32fab750641
Daframp <blakeframps@icloud.com>
2024-10-12 13:28:25 +0100

    added react app

---
+++ Scrabble/react-scrabble/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Scrabble Page</h1>
+      <p>This is a test of embedding a React app.</p>
+    </div>
+  );
+}
+
+export default App;
