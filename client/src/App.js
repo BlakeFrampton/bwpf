251ddfaed85b593d804568aead1f7388206c3fd5
Daframp <blakeframps@icloud.com>
2025-02-09 15:21:12 +0000

    Failed attempt at spotify API

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-// import axios from 'axios';
+import axios from 'axios';
 import './App.css';
 
-//data will be the string we send from our server
-// const apiCall = async () => {
-//   try {
-//     const response = await axios.get("http://localhost:8080/"); // Make sure this URL is correct
-//     console.log(response.data);
-//   } catch (error) {
-//     console.error("Error fetching data:", error);
-//   }
-// };
+// data will be the string we send from our server
+const apiCall = async () => {
+  try {
+    const response = await axios.get("http://localhost:5000/"); // Make sure this URL is correct
+    console.log(response.data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
+      <header className="App-header">
 
         <button onClick={apiCall}>Make API Call</button>
 
-      </header> */}
+      </header>
     </div>
   );
 }
